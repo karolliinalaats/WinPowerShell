02885f70db658d868701a2da3b198d90c28405c2
karolliinalaats <karol-liina.laats@voco.ee>
2026-02-03 09:46:48 +0200

    Kolmas massiiv kuvab esimese ja teise massiivi vastavate väärtuste summa.

---
+++ Massiivid.ps1
@@ -0,0 +1,8 @@
+﻿$Array1 = @(1, 2, 3)
+$Array2 = @(4, 5, 6)
+
+# Loome tühja massiivi ja täidame selle tsükliga
+$Array3 = for($i=0; $i -lt 3; $i++) { $Array1[$i] + $Array2[$i] }
+
+# Kontroll
+$Array3
