9b1de2853bcd1ee0a0c1f746567f895179bc7566
karolliinalaats <karol-liina.laats@voco.ee>
2026-02-09 20:27:06 +0200

    Harjutus 2: Kolme hashtable-i loomine ja automaatne palgaarvutus.

---
+++ Hastable.ps1
@@ -0,0 +1,21 @@
+﻿# 1. Loo Hashtable 1 (Töötatud päevad)
+$Hashtable1 = @{"John"=12; "Joe"=20; "Mary"=18}
+
+# 2. Loo Hashtable 2 (Päevatasu)
+$Hashtable2 = @{"John"=100; "Joe"=120; "Mary"=150}
+
+# 3. Loo Hashtable 3 ja arvuta väärtused
+$Hashtable3 = @{}
+$Hashtable1.Keys | % { 
+    $Hashtable3[$_] = $Hashtable1[$_] * $Hashtable2[$_] 
+}
+
+# VÄLJUND: Kutsume välja kõik kolm tabelit
+Write-Host "--- Hashtable 1 ---"
+$Hashtable1
+
+Write-Host "`n--- Hashtable 2 ---"
+$Hashtable2
+
+Write-Host "`n--- Hashtable 3 (Arvutatud) ---"
+$Hashtable3
