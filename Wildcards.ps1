845e667809070ed4c1e279d81d53949fcf52c9bf
karolliinalaats <karol-liina.laats@voco.ee>
2026-02-10 17:30:22 +0200

    Wildcards kasutamine teenuste otsimisel.

---
+++ Wildcards.ps1
@@ -0,0 +1,11 @@
+﻿# 1. Otsime teenuseid, mis algavad tähega 'S'
+Write-Host "--- Teenused, mis algavad S-iga ---"
+Get-Service S*
+
+# 2. Otsime teenuseid, mille nimes on 'Net' (tärnid mõlemas otsas)
+Write-Host "`n--- Teenused, mille nimes on 'Net' ---"
+Get-Service *Net*
+
+# 3. Kasutame vahemikku: teenused, mis algavad A, B või C-ga
+Write-Host "`n--- Teenused vahemikus A kuni C ---"
+Get-Service [a-c]*
