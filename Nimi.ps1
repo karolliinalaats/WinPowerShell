b3c78f8e83236a4c640750c4a93168aba0e267c1
karolliinalaats <karol-liina.laats@voco.ee>
2026-02-10 17:44:33 +0200

    Harjutus 3: Read-Host ja Write-Host kasutamine nime väljastamiseks.

---
+++ Nimi.ps1
@@ -0,0 +1,5 @@
+﻿# 1. Küsi kasutajalt nime ja salvesta see muutujasse $Nimi
+$Nimi = Read-Host -Prompt "What is your name"
+
+# 2. Väljasta vastus rohelise värviga
+Write-Host "Your name is: $Nimi" -ForegroundColor Green
