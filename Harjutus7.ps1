7bdf19a4cb5f6b6343f9b018c1cc74cbc68d0cba
karolliinalaats <karol-liina.laats@voco.ee>
2026-02-17 12:51:23 +0200

    Harjutus 7: suvaliste gruppide määramine.

---
+++ Harjutus7.ps1
@@ -0,0 +1,17 @@
+﻿# 1. Defineerime värvid
+$varvid = @("Red", "Green", "Yellow", "Blue")
+
+# 2. Loome 20 õpilast ja määrame suvalise värvi
+$tulemus = 1..20 | ForEach-Object {
+    [PSCustomObject]@{
+        RollNumber = $_
+        Group      = Get-Random -InputObject $varvid
+    }
+}
+
+# 3. Kuvame tabeli ekraanil
+Write-Host "Õpilaste jaotus gruppidesse:" -ForegroundColor Cyan
+$tulemus | Format-Table -AutoSize
+
+# 4. Salvestame tulemuse CSV-faili (valikuline, aga soovitatav)
+$tulemus | Export-Csv -Path "grupi_jaotus.csv" -NoTypeInformation -Encoding utf8
