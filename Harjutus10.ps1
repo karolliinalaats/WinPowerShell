2cf3e2cd5c9481aa0406f6597e267ddd633c3f53
karolliinalaats <karol-liina.laats@voco.ee>
2026-02-18 14:16:30 +0200

    Harjutus 10: do-until tsükkel.

---
+++ Harjutus10.ps1
@@ -0,0 +1,16 @@
+﻿<#
+.SYNOPSIS
+    Practice 10: Monitorib Notepad protsesse kasutades do-until tsüklit.
+#>
+
+$counter = 0
+
+do {
+    Write-Host "Notepad is running"
+    $counter++
+    Start-Sleep -Seconds 1
+} 
+until (-not (Get-Process -Name "notepad" -ErrorAction SilentlyContinue))
+
+# Kuvame korduste arvu
+Write-Host $counter
