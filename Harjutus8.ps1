c45cd5882a7a342f14a56602912f9d04cf4ab751
karolliinalaats <karol-liina.laats@voco.ee>
2026-02-18 13:23:12 +0200

    Harjutus 8: Skript teavitab kas Notepad on avatud või mitte.

---
+++ Harjutus8.ps1
@@ -0,0 +1,10 @@
+﻿# We use a while loop to check if any 'notepad' processes exist
+while (Get-Process -Name "notepad" -ErrorAction SilentlyContinue) {
+    Write-Host "Notepad is running"
+    
+    # Adding a small sleep prevents the script from consuming 
+    # too much CPU while it waits
+    Start-Sleep -Seconds 1
+}
+
+Write-Host "All instances of Notepad are closed. Loop ended."
