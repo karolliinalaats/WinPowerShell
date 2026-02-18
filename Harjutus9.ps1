4ff17f340fdbd80a5e3abe2667a16d15e61302fe
karolliinalaats <karol-liina.laats@voco.ee>
2026-02-18 14:13:26 +0200

    Harjutus 9: do-while tsükkel.

---
+++ Harjutus9.ps1
@@ -0,0 +1,17 @@
+﻿<#
+.SYNOPSIS
+    Practice 9: Monitorib Notepadi kasutades do-while tsüklit.
+    Kuvab lõpus teate väljastamise kordade arvu.
+#>
+
+$counter = 0
+
+do {
+    Write-Host "Notepad is running"
+    $counter++  # Suurendame loendurit ühe võrra
+    Start-Sleep -Seconds 1
+} 
+while (Get-Process -Name "notepad" -ErrorAction SilentlyContinue)
+
+# Kuvame lõpptulemuse, nagu pildil näidatud
+Write-Host $counter
