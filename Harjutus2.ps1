b8abd2a558bc04d4a05359d48e22e65f7ead3822
karolliinalaats <karol-liina.laats@voco.ee>
2026-02-25 09:41:24 +0200

    Harjutus 2: palgaarvutuse skript.

---
+++ Harjutus2.ps1
@@ -0,0 +1,8 @@
+﻿$DaysWorked = @{"John"=12; "Joe"=20; "Mary"=18}
+$SalaryPerDay = @{"John"=100; "Joe"=120; "Mary"=150}
+$Salary = @{}
+
+# Käime läbi kõik nimed ja arvutame palga ühe reaga
+$DaysWorked.Keys | % { $Salary[$_] = $DaysWorked[$_] * $SalaryPerDay[$_] }
+
+$Salary
